17f03e6b4821f923e62f59f17d22f855ae2ae368
Mustaghis Tech <info.mustaghistech@gmail.com>
2026-05-15 22:11:58 +0500

    working doctor and pharm

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 // FILE: src/pages/Register.jsx
 import { useState, useEffect } from "react";
-import { FiEye, FiEyeOff, FiMail, FiLock, FiArrowLeft, FiShield, FiUpload, FiX, FiCheckCircle } from "react-icons/fi";
+import { FiEye, FiEyeOff, FiMail, FiLock, FiArrowLeft, FiShield, FiUpload, FiX, FiCheckCircle, FiClock } from "react-icons/fi";
 import { FaArrowRight, FaStethoscope } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
