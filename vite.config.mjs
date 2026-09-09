e3dd0f4236199452a3ccaa73124865a3ddd4578c
briamtechnologies <briamtechnologies@gmail.com>
2026-09-09 19:03:41 +0500

    Update code with latest changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,25 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const require = createRequire(import.meta.url);
+
+const axiosEsmPath = path.join(
+  path.dirname(require.resolve("axios/package.json")),
+  "dist/esm/axios.js"
+);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: { "@": path.resolve(__dirname, "src"), events: false },
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      events: false,
+      axios: axiosEsmPath,
+    },
   },
 });
